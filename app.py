2aabc96a3911e23b5454d847f0052163698d429f
Vishnu KS <ksvishnu56@gmail.com>
2018-07-22 08:23:53 +0000

    bug fix

--- app.py
+++ app.py
@@ -73,15 +73,22 @@ def build_subnet(cidr,vpcid,route):
     con = connect(host="mongodb://migrationuser:mygrationtool@localhost:27017/migration?authSource=admin")
     route_table = ec2.RouteTable(route)
     subnet = ec2.create_subnet(CidrBlock=cidr, VpcId=vpcid)
-    BluePrint.objects(subnet=subnet).update(subnet_id=subnet.id)
+    BluePrint.objects(subnet=cidr).update(subnet_id=subnet.id)
     route_table.associate_with_subnet(SubnetId=subnet.id)
     con.close()
 
 def create_machine(subnet_id,ami_id,machine_type):
+    ec2 = boto3.resource('ec2')
+    client = boto3.client('ec2')
+    amiid = ami_id
+    filters = [{'Name':'name','Values':[ami_id]}]
+    response = client.describe_images(Filters=filters)
+    ami_id = response['Images'][0]['ImageId']
     instances = ec2.create_instances(ImageId=ami_id, InstanceType=machine_type, MaxCount=1, MinCount=1, NetworkInterfaces=[{'SubnetId': subnet_id, 'DeviceIndex': 0, 'AssociatePublicIpAddress': True}])
     instances[0].wait_until_running()
     con = connect(host="mongodb://migrationuser:mygrationtool@localhost:27017/migration?authSource=admin")
-    BluePrint.objects(ami_id=ami_id).update(instance_id=instances[0].id)
+    BluePrint.objects(ami_id=amiid).update(instance_id=instances[0].id)
+    BluePrint.objects(ami_id=amiid).update(status='Build completed')
     con.close()
 
 def compu(name,core,ram):
