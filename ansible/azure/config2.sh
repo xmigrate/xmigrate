17e9d8f557d4edf8d86d2c0ae38f84ad9a1b7da6
Vishnu KS <vishnu@adfolks.com>
2020-07-10 13:57:37 +0530

    added azure ubuntu migration

---
+++ ansible/azure/config2.sh
@@ -0,0 +1,17 @@
+touch /etc/cloud/cloud.cfg.d/90-azure.cfg;
+cat > /etc/cloud/cloud.cfg.d/90-azure.cfg << EOF
+system_info:
+   package_mirrors:
+     - arches: [i386, amd64]
+       failsafe:
+         primary: http://archive.ubuntu.com/ubuntu
+         security: http://security.ubuntu.com/ubuntu
+       search:
+         primary:
+           - http://azure.archive.ubuntu.com/ubuntu/
+         security: []
+     - arches: [armhf, armel, default]
+       failsafe:
+         primary: http://ports.ubuntu.com/ubuntu-ports
+         security: http://ports.ubuntu.com/ubuntu-ports
+EOF
---
+++ ansible/payloads/start_dd_azure.py
@@ -0,0 +1,32 @@
+import os
+from mongoengine import *
+import socket
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+db_con_string = getenv("DB")
+
+con = connect(host=db_con_string)
+storage_accnt = getenv("STORAGE")
+access_key = getenv("ACCESS_KEY")
+
+class BluePrint(Document):
+    host = StringField(required=True, max_length=200, unique=True)
+    ip = StringField(required=True, unique=True)
+    subnet = StringField(required=True, max_length=50)
+    network = StringField(required=True, max_length=50)
+    ports = ListField()
+    cores = StringField(max_length=2)
+    cpu_model = StringField(required=True, max_length=150)
+    ram = StringField(required=True, max_length=50)
+    machine_type = StringField(required=True, max_length=150)
+    status = StringField(required=False, max_length=100)
+    ami_id = StringField(required=False, max_length=100)
+
+BluePrint.objects(host=socket.getfqdn('0.0.0.0')).update(status='Started cloning')
+os.system('sudo dd if=/dev/xvda bs=1M status=progress | azbak - /osdisks/$HOSTNAME.raw --storage-account '+storage_accnt+' --access-key '+access_key)
+
+BluePrint.objects(host=socket.getfqdn('0.0.0.0')).update(status='Completed cloning')
+con.close()
