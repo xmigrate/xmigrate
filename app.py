2f76f8af88048aeef9b575f4e3e07a5fea09b3d3
iamvishnuks <ksvishnu56@gmail.com>
2020-07-13 20:58:00 +0530

    added project route

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from routes.stream import *
+from routes.status import *
+from routes.server import *
+from routes.index import *
+from routes.discover import *
+from routes.cloning import *
+from routes.build import *
+from routes.blueprint import *
+from routes.project import *
 from dotenv import load_dotenv
 from os import getenv
 from ansible.playbook import Playbook
@@ -8,7 +17,6 @@ from pygtail import Pygtail
 from collections import defaultdict
 import boto3
 from flask import Flask
-
 
 import sys
 
@@ -23,14 +31,6 @@ app.secret_key = getenv("SECRET")
 
 bucket_name = getenv("BUCKET")
 
-from routes.blueprint import *
-from routes.build import *
-from routes.cloning import *
-from routes.discover import *
-from routes.index import *
-from routes.server import *
-from routes.status import *
-from routes.stream import *
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000,debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
