7dd2c5de72721412ea79661b63a357bf4ae84d1d
Nihil <nihil@Nihils-MacBook-Pro.local>
2020-07-16 21:37:21 +0530

    stream condition changes

--- routes/stream.py
+++ routes/stream.py
@@ -10,8 +10,9 @@ def stream():
     blueprint_status = ''
     if "PLAY RECAP" in line:
         offset = "EOF"
-        if "unreachable=0 failed=0" in line:
-            blueprint_status = "success"
+        if "unreachable=0" in line:
+            if "failed=0" in line:
+                blueprint_status = "success"
         else:
             blueprint_status = "failure" 
     return jsonify({'line':line,'offset':offset, 'blueprint_status':blueprint_status})
