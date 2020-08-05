c5863c62ba66c339b2502b2953e4ceabe0a0ed30
iamvishnuks <ksvishnu56@gmail.com>
2020-08-05 20:22:32 +0530

    added subnet and network creation modules

---
+++ model/network.py
@@ -0,0 +1,16 @@
+from mongoengine import *
+
+class Network(Document):
+    cidr = StringField(required=True, max_length=50)
+    nw_id = StringField(required=False, max_length=100, unique=True)
+    project = StringField(required=True, max_length=50,unique=True)
+    nw_name = StringField(required=True, max_length=50,unique=True)
+
+
+class Subnet(Document):
+    cidr = StringField(required=True, max_length=50)
+    nw_name = StringField(required=False, max_length=100)
+    project = StringField(required=True, max_length=50,unique=True)
+    subnet_name = StringField(required=True, max_length=50,unique=True)
+    subnet_id = StringField(required=True, max_length=50,unique=True)
+    subnet_type = StringField(required=True, max_length=10)
