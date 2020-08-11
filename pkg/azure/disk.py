cc53dabf531696330c858d754765b1436aea64e3
iamvishnuks <ksvishnu56@gmail.com>
2020-08-11 22:57:39 +0530

    hot patch 2

--- pkg/azure/disk.py
+++ pkg/azure/disk.py
@@ -59,6 +59,7 @@ async def start_cloning(project):
     con.close()
     return False
 
+
 async def create_disk_worker(project,rg_name,uri,disk_name,location,f):
     con = create_db_con()
     compute_client = get_client_from_cli_profile(ComputeManagementClient)
