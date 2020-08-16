276548af1a4bdc5c5d78b52a18d320caf6915da2
iamvishnuks <ksvishnu56@gmail.com>
2020-08-16 15:49:14 +0530

    subnet_id name too long error

--- model/network.py
+++ model/network.py
@@ -11,6 +11,6 @@ class Subnet(Document):
     cidr = StringField(required=True, max_length=50,unique=True)
     nw_name = StringField(required=False, max_length=100)
     project = StringField(required=True, max_length=50)
-    subnet_name = StringField(required=True, max_length=50,unique=True)
-    subnet_id = StringField(required=True, max_length=50,unique=True)
+    subnet_name = StringField(required=True, max_length=150,unique=True)
+    subnet_id = StringField(required=True, max_length=150,unique=True)
     subnet_type = StringField(required=True, max_length=10)
