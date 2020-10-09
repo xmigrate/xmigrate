190e2980ea6937bf8725d12adb058c3922c3eeb0
Vishnu KS <ksvishnu56@gmail.com>
2020-10-09 19:55:19 +0530

    subnet_id removed
    subnet_id = StringField(required=True, max_length=250,unique=True)

--- model/network.py
+++ model/network.py
@@ -11,5 +11,4 @@ class Subnet(Document):
     nw_name = StringField(required=False, max_length=100)
     project = StringField(required=True, max_length=50)
     subnet_name = StringField(required=True, max_length=150,unique=True)
-    subnet_id = StringField(required=True, max_length=250,unique=True)
     subnet_type = StringField(required=True, max_length=10)
