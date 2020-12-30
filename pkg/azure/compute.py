1bf0c4f66190a26d3cb958cbb5d4b0c34e368500
iamvishnuks <ksvishnu56@gmail.com>
2020-12-30 16:23:51 +0530

    fixed data type issue

--- pkg/azure/compute.py
+++ pkg/azure/compute.py
@@ -48,7 +48,7 @@ def create_vm_worker(rg_name, vm_name, location, username, password, vm_type, ni
     print("Provisioned virtual machine")
     try:
         con = create_db_con()
-        BluePrint.objects(project=project, host=vm_name).update(vm_id=vm_result.name,status=100)
+        BluePrint.objects(project=project, image_id=image_name).update(vm_id=vm_result.name,status='100')
     except Exception as e:
         print("VM creation updation failed: "+repr(e))
         logger("VM creation updation failed: "+repr(e),"warning")
