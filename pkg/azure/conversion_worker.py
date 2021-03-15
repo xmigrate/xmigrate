ebea185144f355a3ca8d5334005a681029554cd8
iamvishnuks <ksvishnu56@gmail.com>
2021-03-15 23:08:44 +0530

    Added download worker function

--- pkg/azure/conversion_worker.py
+++ pkg/azure/conversion_worker.py
@@ -67,30 +67,34 @@ async def upload_worker(osdisk_raw,project,host):
 
 
 async def conversion_worker(osdisk_raw,project,host):
-    con = create_db_con()
-    account_name = Storage.objects(project=project)[0]['storage']
-    container_name = Storage.objects(project=project)[0]['container']
-    access_key = Storage.objects(project=project)[0]['access_key']
-    sas_token = sas.generate_sas_token(account_name,access_key)
-    pipe_result = ''
-    try:
-        osdisk_vhd = osdisk_raw.replace(".raw",".vhd")
-        cur_path = os.getcwd()
-        path = cur_path+"/osdisks/"+osdisk_raw
-        vhd_path = cur_path+"/osdisks/"+osdisk_vhd
-        print("Start converting")
-        print(path)
-        os.popen('echo "start converting">> ./logs/ansible/migration_log.txt')
-        command2 = "qemu-img convert -f raw -o subformat=fixed -O vpc "+path+" "+vhd_path
-        process2 = await asyncio.create_subprocess_shell(command2, stdin = PIPE, stdout = PIPE, stderr = STDOUT)
-        await process2.wait()
-        BluePrint.objects(project=project,host=host).update(status='35')
-        os.popen('echo "Conversion completed" >> ./logs/ansible/migration_log.txt')
-    except Exception as e:
-        print(str(e))
-        logger(str(e),"warning")
-        file_size = '0'
-    finally:
-        con.close() 
+    downloaded = await download_worker(osdisk_raw,project,host)
+    if downloaded:
+        con = create_db_con()
+        account_name = Storage.objects(project=project)[0]['storage']
+        container_name = Storage.objects(project=project)[0]['container']
+        access_key = Storage.objects(project=project)[0]['access_key']
+        sas_token = sas.generate_sas_token(account_name,access_key)
+        pipe_result = ''
+        try:
+            osdisk_vhd = osdisk_raw.replace(".raw",".vhd")
+            cur_path = os.getcwd()
+            path = cur_path+"/osdisks/"+osdisk_raw
+            vhd_path = cur_path+"/osdisks/"+osdisk_vhd
+            print("Start converting")
+            print(path)
+            os.popen('echo "start converting">> ./logs/ansible/migration_log.txt')
+            command2 = "qemu-img convert -f raw -o subformat=fixed -O vpc "+path+" "+vhd_path
+            process2 = await asyncio.create_subprocess_shell(command2, stdin = PIPE, stdout = PIPE, stderr = STDOUT)
+            await process2.wait()
+            BluePrint.objects(project=project,host=host).update(status='35')
+            os.popen('echo "Conversion completed" >> ./logs/ansible/migration_log.txt')
+        except Exception as e:
+            print(str(e))
+            logger(str(e),"warning")
+            file_size = '0'
+        finally:
+            con.close() 
+    else:
+        logger("Downloading image failed","warning")
 
 
