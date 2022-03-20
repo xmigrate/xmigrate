3c2aa0a155da9d71098b6c91ee1d3b8be1a50d7d
iamvishnuks <ksvishnu56@gmail.com>
2022-03-20 17:01:34 +0000

    added requests in gcp

--- ansible/payloads/footprint.py
+++ ansible/payloads/footprint.py
@@ -14,14 +14,14 @@ import sys
 import os
 
 
-    niface = 'eth0'
+niface = 'eth0'
 
-    addrs = psutil.net_if_addrs()
+addrs = psutil.net_if_addrs()
 
-    for i in addrs.keys():
-        if addrs[i][0].address.split('.')[0] in ['192', '172', '10']:
-            print(i)
-            niface = i
+for i in addrs.keys():
+    if addrs[i][0].address.split('.')[0] in ['192', '172', '10']:
+        print(i)
+        niface = i
 
 def network_info():
     ifaces = netifaces.interfaces()
