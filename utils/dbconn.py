5e38a81a59380fc9a8658de5c8698cb9a8068a22
iamvishnuks <ksvishnu56@gmail.com>
2022-05-11 16:09:26 +0000

    testing couchdb

--- utils/dbconn.py
+++ utils/dbconn.py
@@ -1,9 +1,17 @@
 from mongoengine import *
 from dotenv import load_dotenv
 from os import getenv
+import couchdb
+
 load_dotenv()
 
 db = getenv("MONGO_DB")
+couch_db = getenv("COUCH_DB")
+
 def create_db_con():
     con = connect(host=db)
+    return con
+
+def create_couchdb_con():
+    con = couchdb.Server(couch_db)
     return con
