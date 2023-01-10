257a695f654a2bba23e85892a3580d7cf0133103
WeepingClown13 <ananthucv5@gmail.com>
2023-01-10 08:56:13 +0000

    changed playbook execution method to python function from cli

--- utils/playbook.py
+++ utils/playbook.py
@@ -6,19 +6,20 @@ from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 from ansible.vars.manager import VariableManager
 
-def run_playbook(username, mongodb_url, project_name, provider=None, curr_working_dir=None, project_id=None, gs_access_key_id=None, gs_secret_access_key=None):
+def run_playbook(provider: str, username: str, project_name: str, curr_working_dir: str, extra_vars: set):
 
     loader = DataLoader()
 
     context.CLIARGS = ImmutableDict(tags={}, listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh',
                         module_path=None, forks=100, remote_user=username, become=True,
-                        become_method='sudo', become_user=username, verbosity=True, check=False, start_at_task=None)
+                        become_method='sudo', become_user=username, verbosity=True, check=False, start_at_task=None, extra_vars=extra_vars)
+    
+    sources = "{}/ansible/{}/hosts".format(curr_working_dir, project_name)
 
-    inventory = InventoryManager(loader=loader, sources=("{current_dir}+/ansible/+{project}+/hosts"))
+    inventory = InventoryManager(loader=loader, sources=(sources))
 
     variable_manager = VariableManager(loader=loader, inventory=inventory, version_info=CLI.version_info(gitinfo=False))
-    variable_manager.extra_vars = {"mongodb": mongodb_url, "project": project_name}
 
-    pbex = PlaybookExecutor(playbooks=['./ansible/aws/xmigrate.yaml'], inventory=inventory, variable_manager=variable_manager, loader=loader, passwords={})
+    pbex = PlaybookExecutor(playbooks=['./ansible/{}/xmigrate.yaml'.format(provider)], inventory=inventory, variable_manager=variable_manager, loader=loader, passwords={})
 
     return pbex.run()
