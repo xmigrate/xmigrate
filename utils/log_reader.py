902e3319e3bd9f6e99fb16c752eb80a05599e942
WeepingClown13 <ananthucv5@gmail.com>
2023-01-24 08:08:03 +0000

    dynamic log reading

--- utils/log_reader.py
+++ utils/log_reader.py
@@ -1,6 +1,6 @@
-def read_logs():
+async def read_logs(curr_working_dir, project_name):
     l = ''
-    f = open('./logs/ansible/log.txt','r')
+    f = open('{}/logs/ansible/{}/log.txt'.format(curr_working_dir ,project_name),'r')
     l = f.read()
     return l
 
