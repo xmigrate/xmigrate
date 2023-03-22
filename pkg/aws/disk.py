c1963e5f7f85c9fdc1735952ba9532c9b5ff309a
akhilprakash634 <akhilprakash634@gmail.com>
2023-03-22 22:01:58 +0530

    disk cloning

--- pkg/aws/disk.py
+++ pkg/aws/disk.py
@@ -20,25 +20,32 @@ async def start_cloning(project, hostname):
     except Exception as e:
         print("Error occurred: "+str(e))
     load_dotenv()
-    
     username = Project.objects(name=project)[0]['username']
     mongodb = os.getenv('BASE_URL')
     current_dir = os.getcwd() 
     playbook = "start_migration.yaml"
     stage = "start clone"
-    provider= 'aws'
-    extra_vars = {'bucket': bucket, 'access_key': access_key, 'secret_key': secret_key, 'public_ip':public_ip, 'user':user, 'mongodb':mongodb, 'project':project, } 
-    cloning_completed = run_playbook(username=username,provider=provider,project_name=project, curr_working_dir=current_dir, playbook=playbook, stage=stage, extra_vars=extra_vars)
-    if cloning_completed:
-        machines = BluePrint.objects(project=project).allow_filtering()
-        machine_count = len(machines)
-        flag = True
-        status_count = 0
-        while flag:
-                for machine in machines:
-                    if int(machine['status'])>=25:
-                        status_count = status_count + 1
-                if status_count == machine_count:
-                    flag = False
-        con.shutdown()
-        return not flag
+    provider = Project.objects(name=project)[0]['provider']
+    
+    # print(project)
+    extra_vars = {'bucket': bucket, 'access_key': access_key, 'secret_key': secret_key, 'mongodb': mongodb, 'project': project, 'public_ip': public_ip, 'user': user} 
+    # cloning_complets = 
+    # print(extra_vars)
+    if hostname == "all":
+        run_playbook(username=username,provider=provider,project_name=project, curr_working_dir=current_dir, playbook=playbook, stage=stage, extra_vars=extra_vars)
+    else:
+        run_playbook(username=username,provider=provider,project_name=project, curr_working_dir=current_dir, playbook=playbook, stage=stage, extra_vars=extra_vars)
+    
+    # if cloning_complets:
+    machines = BluePrint.objects(project=project).allow_filtering()
+    machine_count = len(machines)
+    flag = True
+    status_count = 0
+    while flag:
+            for machine in machines:
+                if int(machine['status'])>=25:
+                    status_count = status_count + 1
+            if status_count == machine_count:
+                flag = False
+    con.shutdown()
+    return not flag
