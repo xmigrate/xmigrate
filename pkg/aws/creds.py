653be38af096d2bb5f22f70a6919c6d70e031449
WeepingClown13 <ananthucv5@gmail.com>
2023-04-20 10:58:41 +0530

    use relative path

--- pkg/aws/creds.py
+++ pkg/aws/creds.py
@@ -8,8 +8,7 @@ def set_aws_creds(project):
         access_key = Project.objects(name=project)[0]['access_key']
         secret_key = Project.objects(name=project)[0]['secret_key']
         location = Project.objects(name=project)[0]['location']
-        user = Project.objects(name=project).allow_filtering()[0]['username']
-        aws_dir = '/home/{}/.aws'.format(user)
+        aws_dir = os.path.expanduser("~/.aws/")
         credentials_str = '['+project+']\naws_access_key_id = '+ access_key+'\n'+ 'aws_secret_access_key = '+secret_key
         if not os.path.exists(aws_dir):
             os.mkdir(aws_dir)
