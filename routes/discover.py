4712801b05499ff2dd8345ceaf93913ced9d6ce3
WeepingClown13 <ananthucv5@gmail.com>
2023-06-09 15:46:52 +0530

    use db functions

--- routes/discover.py
+++ routes/discover.py
@@ -1,56 +1,37 @@
-from model.blueprint import Blueprint
-from model.disk import Disk
-from model.discover import Discover as DiscoverM
-from model.project import Project as ProjectM
+from pkg.aws.aws_config import write_aws_creds
+from routes.auth import TokenData, get_current_user
+from services.blueprint import check_blueprint_exists, create_blueprint, get_blueprintid
+from services.discover import check_discover_exists, create_discover, get_discoverid, update_discover
+from services.disk import check_disk_exists, create_disk
+from services.project import get_projectid, get_project_by_name
+from schemas.discover import DiscoverBase
 from utils.database import dbconn
 from utils.playbook import run_playbook
 from pkg.common import nodes as n
 import os, netaddr, re
-from typing import Union
-from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from sqlalchemy import update
 from sqlalchemy.orm import Session
+
 
 router = APIRouter()
 
-class Discover(BaseModel):
-    provider: Union[str,None] = None
-    hosts: Union[list,None] = None
-    username: Union[str,None] = None
-    password: Union[str,None] = None
-    project: Union[str,None] = None
-
 @router.post('/discover')
-async def discover(data: Discover, db: Session = Depends(dbconn)):
+async def discover(data: DiscoverBase, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
     current_dir = os.getcwd()
-
-    provider = data.provider
-    nodes = data.hosts
-    username = data.username
-    password = data.password
     project = data.project
 
-    if n.add_nodes(nodes, username, password, project, db) == False:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail=jsonable_encoder({"msg": "Request couldn't process"}))
+    if n.add_nodes(data.hosts, data.username, data.password, project, db) == False:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=jsonable_encoder({"msg": "request couldn't process"}))
 
     playbook = "gather_facts.yaml"
     stage = "gather_facts"
         
-    if provider == "aws":
-        proj_details = db.query(ProjectM).filter(ProjectM.name==project).first()
-    
-        aws_dir = os.path.expanduser('~/.aws')
-        if not os.path.exists(aws_dir):
-            os.mkdir(aws_dir)
-
-        with open(f'{aws_dir}/credentials', 'w+') as cred, open(f'{aws_dir}/config', 'w+') as config:
-            cred.write(f'[{project}]\naws_access_key_id = {proj_details.access_key}\naws_secret_access_key = {proj_details.secret_key}')
-            config.write(f'[profile {project}]\nregion = {proj_details.location}\noutput = json')
+    if data.provider == "aws":
+        proj_details = get_project_by_name(current_user['username'], project, db)
+        write_aws_creds(project, proj_details.aws_access_key, proj_details.aws_secret_key, proj_details.location)
     try:
-        finished, output = run_playbook(provider=provider, username=username, project_name=project, curr_working_dir=current_dir, playbook=playbook, stage=stage)
+        finished, output = run_playbook(provider=data.provider, username=data.username, project_name=project, curr_working_dir=current_dir, playbook=playbook, stage=stage)
         if finished:
             if 'ok' in output.stats.keys():
                 linux_host = list(output.stats['ok'].keys())[0]
@@ -64,13 +45,12 @@ async def discover(data: Discover, db: Session = Depends(dbconn)):
                                 disk_details = e['event_data']['res']['disk_info.stdout_lines']
                             if "blkid.stdout_lines" in e['event_data']['res'].keys():
                                 blkid = e['event_data']['res']['blkid.stdout_lines']
-                hostname = facts['ansible_hostname']
+                hostname = str(facts['ansible_hostname'])
                 ip_address = facts['ansible_all_ipv4_addresses'][0]
                 nwinfo = netaddr.IPNetwork(f'{facts["ansible_default_ipv4"]["address"]}/{facts["ansible_default_ipv4"]["netmask"]}')
                 subnet = str(nwinfo)
                 network = str(nwinfo.network)
-                ports = []
-                cores = str(facts['ansible_processor_cores'])
+                cores = int(facts['ansible_processor_cores'])
                 cpu_model = facts['ansible_processor'][2] if len(facts['ansible_processor']) > 2 and len(facts['ansible_processor'][2]) > 1 else ' '
                 ram = str(facts['ansible_memtotal_mb'])
                 disks = []
@@ -95,31 +75,26 @@ async def discover(data: Discover, db: Session = Depends(dbconn)):
                         hashmap['dev'] = f'/dev/{hashmap["dev"]}'
                         disks.append(hashmap)
                 try:
-                    if db.query(DiscoverM).filter(DiscoverM.project==project,DiscoverM.host==hostname).count() == 0:
-                        dscvr = DiscoverM(project=project, host=hostname, ip=ip_address, subnet=subnet, network=network, ports=ports, cores=cores, cpu_model=cpu_model, ram=ram, disk_details=disks, public_ip=','.join(nodes))
-                        db.add(dscvr)
-                        db.commit()
-                        db.refresh(dscvr)
+                    project_id = get_projectid(current_user['username'], project, db)
+                    discover_data = project_id, hostname, network, subnet, None, cores, cpu_model, ram, disks, ip_address
+                    discover_exists = check_discover_exists(project_id, db)
+                    if not discover_exists:
+                        create_discover(discover_data, db)
                     else:
-                        db.execute(update(DiscoverM).where(
-                            DiscoverM.project==project and DiscoverM.host==hostname
-                            ).values(
-                            ip=ip_address, subnet=subnet, network=network, ports=ports, cores=cores, cpu_model=cpu_model, ram=ram, disk_details=disks
-                            ).execution_options(synchronize_session="fetch"))
-                        db.commit()
+                        discover_id = get_discoverid(project_id, db)
+                        update_discover(discover_id, discover_data, db)
 
-                    if db.query(Blueprint).filter(Blueprint.project==project,Blueprint.host==hostname).count() == 0:
-                        blrpnt = Blueprint(project=project, host=hostname, ip=ip_address, subnet=subnet, network=network, cpu_model=cpu_model, ram=ram, machine_type=' ')
-                        db.add(blrpnt)
-                        db.commit()
-                        db.refresh(blrpnt)
+                    blueprint_exists = check_blueprint_exists(project_id, db)
+                    if not blueprint_exists:
+                        create_blueprint(project_id, db)
 
-                    if db.query(Disk).filter(Disk.project==project, Disk.host==hostname).count() == 0:
-                        for disk in disks:
-                            dsk = Disk(project=project, host=hostname, mnt_path=disk['mnt_path'].replace('/', 'slash'), vhd=' ', file_size='0', disk_id=' ')
-                            db.add(dsk)
-                            db.commit()
-                            db.refresh(dsk)
+                    blueprint_id = get_blueprintid(project_id, db)
+                    for disk in disks:
+                        mnt_path = disk['mnt_path'].replace('/', 'slash')
+                        disk_exists = check_disk_exists(blueprint_id, mnt_path, db)
+                        if not disk_exists:
+                            disk_data = hostname, None, '0', mnt_path, None, blueprint_id
+                            create_disk(disk_data, db)
                 except Exception as e:
                     print("Error: "+str(e))
             return jsonable_encoder({'status': '200'})
@@ -128,5 +103,4 @@ async def discover(data: Discover, db: Session = Depends(dbconn)):
             return jsonable_encoder({'status': '400'})
     except Exception as e:
         print(str(e))
-        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=jsonable_encoder(
-            {"msg": "Request couldn't process"}))
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=jsonable_encoder({"msg": "Request couldn't process"}))
--- routes/vm_types.py
+++ routes/vm_types.py
@@ -1,24 +1,16 @@
-from model.project import Project
-from pkg.aws import ec2
-from pkg.azure import compute
-from pkg.gcp import compute as gce
+from pkg.common.vm_types import get_vm_types
+from routes.auth import get_current_user, TokenData
 from utils.database import dbconn
 from fastapi import Depends, APIRouter
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
+
 router = APIRouter()
 
 @router.get('/vms')
-async def vms_get(project: str, db: Session = Depends(dbconn)):
-    provider = (db.query(Project).filter(Project.name==project).first()).provider
-
-    if provider == 'azure':
-        machine_types, flag = compute.get_vm_types(project, db)
-    elif provider == 'aws':
-        machine_types, flag = ec2.get_vm_types(project, db)
-    elif provider == 'gcp':
-        machine_types, flag = gce.get_vm_types(project, db)
+async def vms_get(project: str, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    machine_types, flag = get_vm_types(current_user['username'], project, db)
     if flag:
         return jsonable_encoder({'status': '200', 'machine_types': machine_types})
     else:
