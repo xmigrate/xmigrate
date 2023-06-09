f61116158db9e7e637d88b7ddeb95b6cf6c1bc24
WeepingClown13 <ananthucv5@gmail.com>
2023-06-09 15:46:01 +0530

    fix imports

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-# Model imports are for Base to get table context, do not cleanup even if unused!!!
-# The order is important to create foreign key relations
-from model import user, project, storage, discover, blueprint, disk, network
+# Model imports are for fetching table contexts, they are unused here but is necessary.
+# The order of model imports needs to be preserved for proper foreign key relations.
+# The context can also be loaded from inside the imports given in routes but,
+# they needn't necessarily be ordered as required, hence the explicit import.
+from model import user, project, mapping, storage, discover, blueprint, disk
 # import routes with alias to avoid overrides
-from routes import (
-    auth,
-    blueprint as blueprint_r,
-    discover as discover_r,
-    locations,
-    master,
-    project as project_r,
-    status,
-    storage as storage_r,
-    stream,
-    vm_types
-    )
+from routes import (auth,
+                    blueprint as blueprint_r,
+                    discover as discover_r,
+                    locations, master,
+                    project as project_r,
+                    status, storage as storage_r,
+                    stream,
+                    vm_types)
 from utils.database import Base, engine
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
