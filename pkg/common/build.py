0f8239cf42ef0ad63cad90071e828569129b8622
WeepingClown13 <ananthucv5@gmail.com>
2023-06-19 15:03:23 +0530

    prepare and clone fixes

--- pkg/common/build.py
+++ pkg/common/build.py
@@ -1,110 +1,52 @@
-from model.blueprint import Blueprint
 from model.project import Project
 from model.storage import GcpBucket
 from pkg.aws import ami
-from pkg.aws import disk as awsdisk
 from pkg.aws import ec2
-from pkg.aws import network as awsnw
+from pkg.aws.network import create_nw as aws_create_nw
 from pkg.azure import compute
 from pkg.azure import disk
-from pkg.azure import network
-from pkg.azure import resource_group
-from pkg.common import nodes as n
+from pkg.azure.network import create_nw as azure_create_nw
+from pkg.common.cloning import clone
+from pkg.common.vm_preparation import prepare
 from pkg.gcp import compute as gcp_compute
 from pkg.gcp import disk as gcpdisk
-from pkg.gcp import network as gcpnw
+from pkg.gcp.network import create_nw as gcp_create_nw
+from services.project import get_project_by_name
 from utils.logger import *
-from utils.playbook import run_playbook
 import asyncio
-import os
-from sqlalchemy import update
 
 
-async def call_start_vm_preparation(project, hostname, db):
-    await asyncio.create_task(start_vm_preparation(project, hostname, db))
+async def call_start_vm_preparation(user, project, hostname, db) -> None:
+    await asyncio.create_task(start_vm_preparation(user, project, hostname, db))
 
-async def start_vm_preparation(project, hostname, db):
-    p = db.query(Project).filter(Project.name==project).first()
 
-    if p is not None:
-        nodes = []
-        
-        for ip in p.public_ip:
-            nodes.append(ip)
-            
-        if n.add_nodes(nodes, p.username, p.password, project, db, False) == False:
-            logger("VM preparation couldn't start because inventory was not created","error")
-        else:
-            playbook = "xmigrate.yaml"
-            stage = "vm_preparation"
-            curr_dir = os.getcwd()
+async def start_vm_preparation(user, project, hostname, db) -> None:
+    logger("VM preparation started","info")
+    print("****************VM preparation awaiting*****************")
+    preparation_completed = await prepare(user, project, hostname, db)
+    if preparation_completed:
+        print("****************VM preparation completed*****************")
+        logger("VM preparation completed", "info")
+    else:
+        print("VM preparation failed")
+        logger("VM preparation failed", "error")
 
-            if p.provider == "gcp":
-                storage = db.query(GcpBucket).filter(GcpBucket.project==project).first()
 
-            extra_vars = {
-                'project_id': storage.project_id,
-                'gs_access_key_id': storage.access_key,
-                'gs_secret_access_key': storage.secret_key
-            } if p.provider == "gcp" else None
+async def call_start_clone(user, project, hostname, db) -> None:
+    await asyncio.create_task(start_cloning(user, project,hostname, db))
 
-            logger("VM preparation started", "info")
-            print("****************VM preparation awaiting*****************")
-            try:
-                preparation_completed = run_playbook(provider=p.provider, username=p.username, project_name=project, curr_working_dir=curr_dir, playbook=playbook, stage=stage, extra_vars=extra_vars)
-            
-                if preparation_completed:                     
-                    db.execute(update(Blueprint).where(
-                        Blueprint.project==project and Blueprint.host==hostname[0]
-                        ).values(
-                        status="21"
-                        ).execution_options(synchronize_session="fetch"))
-                    db.commit()
 
-                    print("****************VM preparation completed*****************")
-                    logger("VM preparation completed", "info")
-                else:
-                    print("VM preparation failed")
-                    logger("VM preparation failed", "error")
-            except Exception as e:
-                print(str(e))
+async def start_cloning(user, project, hostname, db) -> None:
+    logger("Cloning started","info")
+    print("****************Cloning awaiting*****************")
+    cloning_completed = await clone(user, project, hostname, db)
+    if cloning_completed:
+        print("****************Cloning completed*****************")
+        logger("Cloning completed","info")
+    else:
+        print("Disk cloning failed")
+        logger("Disk cloning failed", "error")
 
-async def call_start_clone(project, hostname, db):
-    await asyncio.create_task(start_cloning(project,hostname, db))
-
-async def start_cloning(project, hostname, db):
-    provider = (db.query(Project).filter(Project.name==project).first()).provider
-
-    if provider == "azure":
-        logger("Cloning started","info")
-        print("****************Cloning awaiting*****************")
-        cloning_completed = await disk.start_cloning(project, hostname, db)
-        if cloning_completed:
-            print("****************Cloning completed*****************")
-            logger("Disk cloning completed","info")
-        else:
-            print("Disk cloning failed")
-            logger("Disk cloning failed","error")
-    elif provider == "aws":
-        logger("Cloning started","info")
-        print("****************Cloning awaiting*****************")
-        cloning_completed = await awsdisk.start_cloning(project, hostname, db)
-        if cloning_completed:
-            print("****************Cloning completed*****************")
-            logger("Cloning completed","info")
-        else:
-            print("Disk cloning failed")
-            logger("Disk cloning failed","error")
-    elif provider == "gcp":
-        logger("Cloning started","info")
-        print("****************Cloning awaiting*****************")
-        cloning_completed = await gcpdisk.start_cloning(project, hostname, db)
-        if cloning_completed:
-            print("****************Cloning completed*****************")
-            logger("Cloning completed","info")
-        else:
-            print("Disk cloning failed")
-            logger("Disk cloning failed","error")
 
 async def call_start_convert(project, hostname, db):
     await asyncio.create_task(start_convert(project, hostname, db))
@@ -160,29 +102,23 @@ async def start_convert(project, hostname, db):
                 logger("Disk Conversion failed","error")
 
 
-async def call_build_network(project, db):
-    await asyncio.create_task(start_network_build(project, db))
+async def call_build_network(user, project, db):
+    await asyncio.create_task(start_network_build(user, project, db))
 
-async def start_network_build(project, db):
-    provider = (db.query(Project).filter(Project.name==project).first()).provider
+async def start_network_build(user, project, db):
+    provider = get_project_by_name(user, project, db).provider
 
     if provider == "azure":
         logger("Network build started","info")
         print("****************Network build awaiting*****************")
-        rg_created = await resource_group.create_rg(project, db)
-        if rg_created:
-            logger("Resource group created","info")
-            network_created = await network.create_nw(project, db)
-            if network_created:
-                logger("Network created","info")
-            else:
-                logger("Network creation failed","error")
+        network_created = await azure_create_nw(project, db)
+        if network_created:
+            logger("Network created","info")
         else:
-            print("Resource group creation failed")
-            logger("Resource group creation failed","error")
+            logger("Network creation failed","error")
     elif provider == "aws":
         logger("Network creation started","info")
-        network_created = await awsnw.create_nw(project, db)
+        network_created = await aws_create_nw(user, project, db)
         if network_created:
             logger("Network creation completed","info")
         else:
@@ -190,7 +126,7 @@ async def start_network_build(project, db):
             logger("Network creation failed","error")
     elif provider == "gcp":
         logger("Network creation started","info")
-        network_created = await gcpnw.create_nw(project, db)
+        network_created = await gcp_create_nw(user, project, db)
         if network_created:
             logger("Network creation completed","info")
         else:
--- routes/blueprint.py
+++ routes/blueprint.py
@@ -1,173 +1,141 @@
-from model.blueprint import Blueprint
-from model.discover import Discover
-from utils.database import dbconn
 from pkg.azure import *
-from pkg.common import network as netutils
 from pkg.common import build
 from pkg.common import hosts as host
+from pkg.common.vm_preparation import start_vm_preparation
+from routes.auth import TokenData, get_current_user
+from schemas.common import CommonBase, CommonCreate
+from schemas.network import NetworkCreate, NetworkDelete, SubnetCreate, SubnetDelete
+from services.blueprint import get_blueprintid
+from services.network import (check_network_exists, check_subnet_exists, create_network, create_subnet, delete_network, delete_subnet, get_all_networks, get_all_subnets, get_networkid)
+from services.discover import get_discover
+from services.project import get_projectid
+from utils.database import dbconn
 import asyncio
 from concurrent.futures import ProcessPoolExecutor
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.encoders import jsonable_encoder
+from fastapi.requests import Request
 from pydantic import BaseModel
-from quart import request
-from quart_jwt_extended import jwt_required
-from sqlalchemy import update
 from sqlalchemy.orm import Session
 from typing import Union
+
 
 router = APIRouter()
 executor = ProcessPoolExecutor(max_workers=5)
 
 @router.get('/blueprint')
-async def get_blueprint(project: str, db: Session = Depends(dbconn)):
-    return jsonable_encoder(db.query(Discover).filter(Discover.project==project).all())
+async def get_blueprint(project: str, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    project_id = get_projectid(current_user['username'], project, db)
+    return get_discover(project_id, db)
 
-class NetworkCreate(BaseModel):
-    project: Union[str,None] = None
-    name: Union[str,None] = None
-    cidr: Union[str,None] = None
 
 @router.post('/blueprint/network')
-async def create_network(data: NetworkCreate, db: Session = Depends(dbconn)):
-    network = data.cidr if data.cidr !=None else ''
-    project = data.project
-    name = data.name
-
-    network_layout_created = netutils.create_nw(project, name, network, db)
-
-    if network_layout_created:
-        return  jsonable_encoder({'status': '200'})
-    else:
-        return jsonable_encoder({'status': '500', 'msg': 'Network  creation failed'})
+async def network_create(data: NetworkCreate, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    try:
+        data.cidr = data.name if data.cidr is None else data.cidr
+        project_id = get_projectid(current_user['username'], data.project, db)
+        blueprint_id = get_blueprintid(project_id, db)
+        network_exists = check_network_exists(blueprint_id, data.cidr, db)
+        if not network_exists:
+            create_network(blueprint_id, data, db)
+        else:
+            print(f'Network {data.name} already exists for the project!')
+    except Exception as e:
+        print(str(e))
+        return jsonable_encoder({'status': '500', 'msg': 'network  creation failed'})
 
 
 @router.get('/blueprint/network')
-async def get_network(project: str, db: Session = Depends(dbconn)):
-    return  jsonable_encoder(netutils.fetch_nw(project, db))
-
-class NetworkDelete(BaseModel):
-    project: Union[str,None] = None
-    nw_name: Union[str,None] = None
+async def network_get(project: str, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    project_id = get_projectid(current_user['username'], project, db)
+    blueprint_id = get_blueprintid(project_id, db)
+    return get_all_networks(blueprint_id, db)
 
 
 @router.delete('/blueprint/network')
-async def delete_network(req: NetworkDelete, db: Session = Depends(dbconn)):
-    project = req.project
-    nw_name = req.nw_name
-
-    if netutils.delete_nw(project, nw_name, db):
-        return jsonable_encoder({"msg":"success", "status":200})
-    else:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail=jsonable_encoder({"msg": "Request couldn't process"}))
-
-class SubnetDelete(BaseModel):
-    project: Union[str,None] = None
-    subnet_name: Union[str,None] = None
-    nw_name: Union[str,None] = None
+async def network_delete(data: NetworkDelete, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    project_id = get_projectid(current_user['username'], data.project, db)
+    blueprint_id = get_blueprintid(project_id, db)
+    try:
+        delete_network(blueprint_id, data.cidr, db)
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=jsonable_encoder({"msg": "request couldn't process"}))
 
 
 @router.delete('/blueprint/subnet')
-async def delete_subnet(req: SubnetDelete, db: Session = Depends(dbconn)):
-    project = req.project
-    subnet_name = req.subnet_name
-    nw_name = req.nw_name
+async def subnet_delete(data: SubnetDelete, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    try:
+        project_id = get_projectid(current_user['username'], data.project, db)
+        blueprint_id = get_blueprintid(project_id, db)
+        network_id = get_networkid(data.cidr, blueprint_id, db)
+        delete_subnet(network_id, data.subnet_cidr, db)
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=jsonable_encoder({"msg": "request couldn't process"}))
 
-    if netutils.delete_subnet(project, subnet_name, nw_name, db):
-        return jsonable_encoder({"msg":"success", "status":200})
-    else:
-        return jsonable_encoder({"msg":"failed", "status":500})
 
 @router.get('/blueprint/subnet')
-async def get_subnet(network: str, project: str, db: Session = Depends(dbconn)):
-    return  jsonable_encoder(netutils.fetch_subnet(project, network, db))
+async def subnet_get(cidr: str, project: str, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    project_id = get_projectid(current_user['username'], project, db)
+    blueprint_id = get_blueprintid(project_id, db)
+    network_id = get_networkid(cidr, blueprint_id, db)
+    return get_all_subnets(network_id, db)
 
-class SubnetCreate(BaseModel):
-    project: Union[str,None] = None
-    nw_name: Union[str,None] = None
-    cidr: Union[str,None] = None
-    name: Union[str,None] = None
-    nw_type: Union[str,None] = None
 
 @router.post('/blueprint/subnet')
-async def create_subnet(data: SubnetCreate, db: Session = Depends(dbconn)):
-    network = data.cidr
-    project = data.project
-    nw_name = data.nw_name
-    nw_type = data.nw_type
-    name = data.name
-
-    network_layout_created = netutils.create_subnet(network, nw_name, project, nw_type, name, db)
-
-    if network_layout_created:
-        return  jsonable_encoder({'status': '200'})
-    else:
-        return jsonable_encoder({'status': '500', 'msg': 'Subnet  creation failed'})
+async def subnet_create(data: SubnetCreate, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    try:
+        project_id = get_projectid(current_user['username'], data.project, db)
+        blueprint_id = get_blueprintid(project_id, db)
+        network_id = get_networkid(data.nw_cidr, blueprint_id, db)
+        subnet_exists = check_subnet_exists(network_id, data.cidr, db)
+        if not subnet_exists:
+            create_subnet(network_id, data, db)
+        else:
+            print(f'Subnet with cidr {data.cidr} already exists for the network {data.nw_cidr}!')
+    except:
+        return jsonable_encoder({'status': '500', 'msg': 'subnet  creation failed'})
     
 
 @router.get('/blueprint/hosts')
-async def get_hosts(project: str, db: Session = Depends(dbconn)):
-    return jsonable_encoder(host.fetch_all_hosts(project, db))
+async def get_hosts(project: str, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    return jsonable_encoder(host.fetch_all_hosts(current_user['username'], project, db))
 
 
 @router.post('/blueprint/update')
-@jwt_required
-async def update_blueprint(db: Session = Depends(dbconn)):
-    data = await request.get_json()
+async def update_blueprint(request: Request, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    try:
+        data = await request.body()
+        return host.update_hosts(current_user['username'], data['project'], data['machines'], db)
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=jsonable_encoder({"msg": "request couldn't process"}))
 
-    project = data['project']
-    machines = data['machines']
+# class BlueprintCreate(BaseModel):
+#     project: Union[str,None] = None
+#     machines: Union[list,None] = None
 
-    blueprint_updated = host.update_hosts(project, machines, db)
+# @router.post('/blueprint/create')
+# async def create_blueprint(data: BlueprintCreate, db: Session = Depends(dbconn)):
+#     project = data.project
+#     machines = data.machines
+#     for machine in machines:
+#         db.execute(update(Blueprint).where(
+#             Blueprint.host==machine["hostname"] and Blueprint.project==project
+#             ).values(
+#             machine_type=machine["machine_type"], public_route=bool(machine["type"])
+#             ).execution_options(synchronize_session="fetch"))
+#         db.commit()
+#         return jsonable_encoder({"msg":"Succesfully updated","status":200})
 
-    if blueprint_updated:
-        return jsonable_encoder({"msg": "Succesfully updated", "status":200})
-    else:
-        return jsonable_encoder({"msg": "Couldn't update Blueprint", "status":500})
-
-class BlueprintCreate(BaseModel):
-    project: Union[str,None] = None
-    machines: Union[list,None] = None
-
-@router.post('/blueprint/create')
-async def create_blueprint(data: BlueprintCreate, db: Session = Depends(dbconn)):
-    project = data.project
-    machines = data.machines
-    for machine in machines:
-        db.execute(update(Blueprint).where(
-            Blueprint.host==machine["hostname"] and Blueprint.project==project
-            ).values(
-            machine_type=machine["machine_type"], public_route=bool(machine["type"])
-            ).execution_options(synchronize_session="fetch"))
-        db.commit()
-        return jsonable_encoder({"msg":"Succesfully updated","status":200})
-
-class Prepare(BaseModel):
-    project: Union[str, None] = None
-    hostname: Union[list, None] = None
 
 @router.post('/blueprint/host/prepare')
-async def vm_prepare(data: Prepare, db: Session = Depends(dbconn)):
-    project = data.project
-    hostname = data.hostname
-    
-    asyncio.create_task(build.call_start_vm_preparation(project, hostname, db))
- 
-    return jsonable_encoder({"msg": "VM preparation started", "status":200})
+async def vm_prepare(data: CommonCreate, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):    
+    asyncio.create_task(build.start_vm_preparation(current_user['username'], data.project, data.hostname, db))
+    return jsonable_encoder({"message": "vm preparation started", "status": 200})
 
-
-class BlueprintHost(BaseModel):
-    project: Union[str,None] = None
-    hostname: Union[str,None] = None
 
 @router.post('/blueprint/host/clone')
-async def image_clone(data: BlueprintHost, db: Session = Depends(dbconn)):
-    project = data.project
-    hostname = data.hostname
-
-    asyncio.create_task(build.call_start_clone(project, hostname, db))
-
+async def image_clone(data: CommonCreate, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    asyncio.create_task(build.call_start_clone(current_user['username'], data.project, data.hostname, db))
     return jsonable_encoder({"msg": "Cloning started", "status":200})
 
 
@@ -180,15 +148,12 @@ async def image_convert(data: BlueprintHost, db: Session = Depends(dbconn)):
 
     return jsonable_encoder({"msg": "Conversion started", "status":200})
 
-class NetworkBuild(BaseModel):
-    project: Union[str,None] = None
 
 @router.post('/blueprint/network/build')
-async def network_build(req: NetworkBuild, db: Session = Depends(dbconn)):
-    project = req.project
-    asyncio.create_task(build.call_build_network(project, db))
+async def network_build(data: CommonBase, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
+    asyncio.create_task(build.call_build_network(current_user['username'], data.project, db))
 
-    return jsonable_encoder({"msg": "Build started", "status":200})
+    return jsonable_encoder({"message": "build started", "status": 200})
 
 @router.post('/blueprint/host/build')
 async def host_build(data: BlueprintHost, db: Session = Depends(dbconn)):
