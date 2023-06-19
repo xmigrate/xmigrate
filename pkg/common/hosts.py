749a6f13f400239c34d766615dbf7a9d9a9b5922
WeepingClown13 <ananthucv5@gmail.com>
2023-06-19 15:00:08 +0530

    fixup hosts data

--- pkg/common/hosts.py
+++ pkg/common/hosts.py
@@ -1,83 +1,36 @@
-from model.blueprint import Blueprint
-from model.network import Network, Subnet
-from model.project import Project
-from sqlalchemy import update
-
-def fetch_hosts(project, db):
-    try:
-        response = {}
-        sub_hosts = {}
-        hosts = db.query(Blueprint).filter(Blueprint.project==project).all()
-
-        for host in hosts:
-            subnet_name = (db.query(Subnet).filter(Subnet.project==project, Subnet.cidr==host.subnet).first()).subnet_name
-            if subnet_name not in sub_hosts.keys():
-                sub_hosts[subnet_name] = []
-
-            machine = dict(host)
-            del(machine['_id'])
-            sub_hosts[subnet_name].append(machine)
-
-        for subnet in sub_hosts.keys():
-            nw_name = (db.query(Subnet).filter(Subnet.project==project, Subnet.subnet_name==subnet).first()).nw_name
-            if nw_name not in response.keys():
-                response[nw_name] = []
-            response[nw_name].append(sub_hosts[subnet])
-        return response
-    except Exception as e:
-        print("Reading from db failed: "+repr(e))
-        return {"msg":"Failed fetching details"}
+from services.blueprint import get_blueprintid
+from services.machines import get_machineid, update_vm
+from services.network import get_all_networks, get_all_subnets, get_networkid
+from services.project import get_projectid
+from schemas.machines import VMUpdate
 
 
-def update_hosts(project, machines, db):
+def update_hosts(user, project, machines, db):
     try:
         for machine in machines:
-            subnet = db.query(Subnet).filter(Subnet.project==project, Subnet.cidr==machine["subnet"]).first()
-            network = db.query(Network).filter(Network.project==project, Network.nw_name==subnet.nw_name).first()
-            provider = (db.query(Project).filter(Project.name==project).first()).provider
-            if machine['public_route'] == 'Public':
-                machine['public_route'] = True
-            elif machine['public_route'] == 'Private':
-                machine['public_route'] = False
-            if provider == 'gcp':
-                db.execute(update(Blueprint).where(
-                    Blueprint.project==project and Blueprint.host==machine['host']
-                    ).values(
-                    machine_type=machine['machine_type'], public_route=machine['public_route'], subnet=machine['subnet'], network=network.nw_name
-                    ).execution_options(synchronize_session="fetch"))
-                db.commit()
-            else:
-                db.execute(update(Blueprint).where(
-                    Blueprint.project==project and Blueprint.host==machine['host']
-                    ).values(
-                    machine_type=machine['machine_type'], public_route=machine['public_route'], subnet=machine['subnet'], network=network.cidr
-                    ).execution_options(synchronize_session="fetch"))
-                db.commit()
+            project_id = get_projectid(user, project, db)
+            blueprint_id = get_blueprintid(project_id, db)
+            public_route = True if machine['public_route'] == 'Public' else False
+            machine_id = get_machineid(machine["hostname"], blueprint_id, db)
+            vm_data = VMUpdate(machine_id=machine_id, machine_type=machine['machine_type'], public_route=public_route, network=machine["network"])
+            update_vm(vm_data, db)
         return True
     except Exception as e:
         print(repr(e))
         return False
 
 
-def fetch_all_hosts(project, db):
+def fetch_all_hosts(user, project, db):
     try:
-        subnets = db.query(Subnet).filter(Subnet.project==project).all()
-        networks = db.query(Network).filter(Network.project==project).all()
-        subnet_object = []
+        project_id = get_projectid(user, project, db)
+        blueprint_id = get_blueprintid(project_id, db)
+        networks = get_all_networks(blueprint_id, db)
         network_objects = []
-        for subnet in subnets:
-            host_objects = [] 
-            hosts = db.query(Blueprint).filter(Blueprint.project==project, Blueprint.subnet==subnet.cidr).all()
-            for host in hosts:
-                host_objects.append(host)
-            subnet_object.append({"name": subnet.subnet_name, "cidr": subnet.cidr, "subnet_type": subnet.subnet_type, "nw_name": subnet.nw_name, "hosts": host_objects})
         for network in networks:
-            subs = []
-            for subnet in subnet_object:
-                if subnet["nw_name"] == network.nw_name:
-                    subs.append(subnet)
-            network_objects.append({"nw_name": network.nw_name, "cidr": network.cidr, 'subnets': subs})
+            network_id = get_networkid(network.name, blueprint_id, db)
+            subnets = get_all_subnets(network_id, db)
+            network_objects.append({"nw_name": network.name, "cidr": network.cidr, 'subnets': subnets})
         return {'networks': network_objects}
     except Exception as e:
-        print(e)
+        print(str(e))
 
