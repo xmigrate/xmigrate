a64f040276dedd86d4622c52cb920120ddd4a6a6
WeepingClown13 <ananthucv5@gmail.com>
2023-07-10 10:04:51 +0000

    retrieve machine data correctly

--- routes/status.py
+++ routes/status.py
@@ -1,5 +1,6 @@
 from routes.auth import TokenData, get_current_user
-from services.blueprint import get_blueprint
+from services.blueprint import get_blueprintid
+from services.machines import get_all_machines
 from services.project import get_projectid
 from utils.database import dbconn
 from fastapi import Depends, APIRouter
@@ -12,5 +13,6 @@ router = APIRouter()
 @router.get('/migration/status')
 async def migration_status(project: str, current_user: TokenData = Depends(get_current_user), db: Session = Depends(dbconn)):
     project_id = get_projectid(current_user['username'], project, db)
-    machines = get_blueprint(project_id, db)
+    blueprint_id = get_blueprintid(project_id, db)
+    machines = get_all_machines(blueprint_id, db)
     return jsonable_encoder(machines)
