257ff0ce2f8fda6f90affc9c7d1da89f63b6a24c
WeepingClown13 <ananthucv5@gmail.com>
2023-07-10 16:48:36 +0530

    quit awaiting tuple object

--- pkg/common/cloning.py
+++ pkg/common/cloning.py
@@ -59,7 +59,7 @@ async def clone(user: str, project: str, hostname: list, db: Session, settings: 
         'ANSIBLE_LOG_PATH': '{}/logs/ansible/{}/cloning_log.txt'.format(current_dir ,project.name)
     }
 
-    cloned = await run_async(playbook=playbook, inventory=inventory, extravars=extravars, envvars=envvars, limit=public_ip, quiet=True)
+    cloned = run_async(playbook=playbook, inventory=inventory, extravars=extravars, envvars=envvars, limit=public_ip, quiet=True)
     
     if (not (bool(cloned[1].stats['failures']) or bool(cloned[1].stats['dark']))):
         blueprint_id = get_blueprintid(project.id, db)
