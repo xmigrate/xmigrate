a3aa66746acb7efdd13ccdff37036d152331eed5
WeepingClown13 <ananthucv5@gmail.com>
2023-07-24 15:49:58 +0530

    fix id gen default param value length

--- utils/id_gen.py
+++ utils/id_gen.py
@@ -1,6 +1,6 @@
 import uuid
 
-def unique_id_gen(name: str = "DEFAULT") -> str:
+def unique_id_gen(name: str = "DEF") -> str:
     """
     creates a sha-1 encoded uuid with the name and returns it as a string.
     
