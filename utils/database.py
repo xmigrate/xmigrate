0d839d94e9fed101afeae0d982a294ab96afc4da
WeepingClown13 <ananthucv5@gmail.com>
2023-08-08 10:58:56 +0530

    fix try catch not catching db connection error

--- utils/database.py
+++ utils/database.py
@@ -1,4 +1,3 @@
-from utils.logger import Logger
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,14 +8,8 @@ password = os.getenv("DB_PASS")
 db_name = os.getenv("DB_NAME")
 db_url = os.getenv("DB_URL")
 
-try:
-    SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{db_url}/{db_name}"
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
-except:
-    Logger.critical("Unable to establish database connection!")
-    raise ConnectionError(f"Cannot connect to {SQLALCHEMY_DATABASE_URL}")
-else:
-    Logger.info("Database connection established")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{db_url}/{db_name}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 LocalSession = sessionmaker(bind=engine, expire_on_commit=False)
 Base = declarative_base()
